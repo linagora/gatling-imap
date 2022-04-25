25806925d122b7f0472b2ef1e4e4eb4ffd887736
Quan Tran <hqtran@linagora.com>
2022-04-25 15:09:11 +0700

    ISSUE-133 Adding COPY command

--- src/main/scala-2.12/com/linagora/gatling/imap/protocol/ImapSessions.scala
+++ src/main/scala-2.12/com/linagora/gatling/imap/protocol/ImapSessions.scala
@@ -156,6 +156,9 @@ private class ImapSession(client: => ImapAsyncClient, protocol: ImapProtocol) ex
     case cmd@Command.Move(_, _, _) =>
       val handler = context.actorOf(MoveHandler.props(session), genName("move"))
       handler forward cmd
+    case cmd@Command.Copy(_, _, _) =>
+      val handler = context.actorOf(CopyHandler.props(session), genName("copy"))
+      handler forward cmd
     case cmd@Command.Status(_, _, _) =>
       val handler = context.actorOf(StatusHandler.props(session), genName("status"))
       handler forward cmd
