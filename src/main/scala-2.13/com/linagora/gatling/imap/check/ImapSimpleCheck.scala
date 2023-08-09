09b2df7c221ef9fb42178a08bbbef1f269f76825
Tung Tran <vttran@linagora.com>
2023-08-09 08:59:01 +0700

    Migrate gatling 3.9.5: adapt io.gatling.core.check.Check

--- src/main/scala-2.13/com/linagora/gatling/imap/check/ImapSimpleCheck.scala
+++ src/main/scala-2.13/com/linagora/gatling/imap/check/ImapSimpleCheck.scala
@@ -3,8 +3,8 @@ package com.linagora.gatling.imap.check
 import com.linagora.gatling.imap.protocol.ImapResponses
 import io.gatling.commons.validation.{Failure, Validation}
 import io.gatling.core.check.Check.PreparedCache
-import io.gatling.core.check.CheckResult
-import io.gatling.core.session.Session
+import io.gatling.core.check.{Check, CheckResult}
+import io.gatling.core.session.{Expression, Session}
 
 object ImapSimpleCheck {
   val DefaultMessage = "Imap check failed"
@@ -16,4 +16,8 @@ case class ImapSimpleCheck(validate: ImapResponses => Boolean, errorMessage: Ima
       CheckResult.NoopCheckResultSuccess
     else
       Failure(errorMessage(response))
+
+  override def checkIf(condition: Expression[Boolean]): Check[ImapResponses] = throw new UnsupportedOperationException("checkIf is not supported for ImapSimpleCheck")
+
+  override def checkIf(condition: (ImapResponses, Session) => Validation[Boolean]): Check[ImapResponses] = throw new UnsupportedOperationException("checkIf is not supported for ImapSimpleCheck")
 }
